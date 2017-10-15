a0e864fb0f9dafa03094be54ab08a062e9bafdbf
Polly Sileo <pollysileo@gmail.com>
2017-10-15 13:38:28 -0400

    Add use of helper function to name the sites

--- catch-of-the-day/src/index.js
+++ catch-of-the-day/src/index.js
@@ -12,8 +12,8 @@ const Root = () => {
   return(
     <BrowserRouter>
         <div>
-          <Match exactly pattern ="/" component={StorePicker}/>
-          <Match exactly pattern ="/store/:storeId" component={App}/>
+          <Match exactly pattern="/" component={StorePicker}/>
+          <Match exactly pattern="/store/:storeId" component={App}/>
           <Miss component={NotFound}/>
         </div>
       </BrowserRouter>
